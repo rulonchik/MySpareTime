418d29649b3ead27480e6dbb0a07624119827437
Ruslan Ruziev <ruslan27012001@gmail.com>
2021-08-19 22:05:05 +0400

    SimpleBot refactored

--- src/HyperSkill/SimpleChattyBot/SimpleBot.java
+++ src/HyperSkill/SimpleChattyBot/SimpleBot.java
@@ -3,23 +3,64 @@ package HyperSkill.SimpleChattyBot;
 import java.util.Scanner;
 
 public class SimpleBot {
+    final static Scanner scanner = new Scanner(System.in); // Do not change this line
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        greet("Aid", "2018"); // change it as you need
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
+    }
 
-        System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2021.");
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
+    }
 
+    static void remindName() {
         String name = scanner.nextLine();
-
         System.out.println("What a great name you have, " + name + "!");
+    }
+
+    static void guessAge() {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
 
-        int r3 = scanner.nextInt();
-        int r5 = scanner.nextInt();
-        int r7 = scanner.nextInt();
+    static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
+        }
+    }
 
-        System.out.println("Your age is " + ((r3 * 70 + r5 * 21 + r7 * 15) % 15) + "; that's a good time to start programming!");
+    static void test() {
+        int answer = 0;
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("What is the meaning of life?");
+        System.out.println("1. Eating");
+        System.out.println("2. 42");
+        System.out.println("3. Pi");
+        System.out.println("4. PiPi and PuPu");
+        System.out.println("5. 2,7");
+        while (answer != 5) {
+            answer = scanner.nextInt();
+            if (answer != 5) System.out.println("Please, try again");
+            else System.out.println("Congratulations, have a nice day!");
+        }
+
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!"); // Do not change this text
     }
 }
